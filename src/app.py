6eda334b1c5cd4e290d344fbb9d601a16f72441d
pilarhl20@gmail.com <you@example.com>
2025-04-16 19:48:03 +0200

    lk

--- src/app.py
+++ src/app.py
@@ -1,2 +1,23 @@
 import streamlit as st
 st.write('Hello, world!')
+
+
+from pickle import load
+
+model = load(open("models/naive_bayes_alpha_1-9176382_fit_prior_False_42.sav", "rb"))
+class_dict = {
+    "0": "Iris setosa",
+    "1": "Iris versicolor",
+}
+
+st.title("Iris - Model prediction")
+
+val1 = st.slider("Petal width", min_value = 0.0, max_value = 4.0, step = 0.1)
+val2 = st.slider("Petal length", min_value = 0.0, max_value = 4.0, step = 0.1)
+val3 = st.slider("Sepal width", min_value = 0.0, max_value = 4.0, step = 0.1)
+val4 = st.slider("Sepal length", min_value = 0.0, max_value = 4.0, step = 0.1)
+
+if st.button("Predict"):
+    prediction = str(model.predict([[val1, val2, val3, val4]])[0])
+    pred_class = class_dict[prediction]
+    st.write("Prediction:", pred_class)
